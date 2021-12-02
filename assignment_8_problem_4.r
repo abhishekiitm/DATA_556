6c88b6b50711d5b5289792dc0682ea8a475c5c8a
Abhishek Saini <abhishekiitm@users.noreply.github.com>
2021-12-01 23:49:56 -0800

    Create assignment_8_problem_4.r

---
+++ assignment_8_problem_4.r
@@ -0,0 +1,20 @@
+
+# install.packages("dplyr")
+library(dplyr)
+
+get_total_tickets = function(n_movies, tickets_per_movie) {
+  sum_tickets = sum(rpois(n_movies, tickets_per_movie))
+  return(sum_tickets)
+}
+
+movies_per_year = 700
+tickets_per_movie = 800000
+
+n_reps = 100000
+
+movies_sample = rpois(n_reps, movies_per_year)
+
+df = data.frame(movies_sample = movies_sample)
+df = df %>% rowwise() %>% mutate(sum_tickets_sample = get_total_tickets(movies_sample, tickets_per_movie))
+
+print(mean(df$sum_tickets_sample))
