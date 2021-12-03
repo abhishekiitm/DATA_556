9374326e5641106140e6b9eb4bd7c7b10ac1e56e
Abhishek Saini <abhishekiitm@users.noreply.github.com>
2021-12-03 02:19:03 -0800

    Update assignment_8_problem_4.r

--- assignment_8_problem_4.r
+++ assignment_8_problem_4.r
@@ -10,7 +10,7 @@ get_total_tickets = function(n_movies, tickets_per_movie) {
 movies_per_year = 700
 tickets_per_movie = 800000
 
-n_reps = 100000
+n_reps = 1000000
 
 movies_sample = rpois(n_reps, movies_per_year)
 
@@ -18,3 +18,5 @@ df = data.frame(movies_sample = movies_sample)
 df = df %>% rowwise() %>% mutate(sum_tickets_sample = get_total_tickets(movies_sample, tickets_per_movie))
 
 print(mean(df$sum_tickets_sample))
+
+print(var(df$sum_tickets_sample))
